cc1f4040680d27f19d6bb3e8f5bc07de553b31b3
Max Vivar-Maas <mvivarmaas@gmail.com>
2021-09-28 18:09:50 -0400

    Employee boilerplate

--- src/main/java/com/mvm/kitchenmanager/model/Employee.java
+++ src/main/java/com/mvm/kitchenmanager/model/Employee.java
@@ -4,4 +4,30 @@ public class Employee {
     private Role role;
     private Double hourlyRate;
     String name;
+
+    public Employee(Role role, Double hourlyRate, String name) {
+        this.role = role;
+        this.hourlyRate = hourlyRate;
+        this.name = name;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public Double getHourlyRate() {
+        return hourlyRate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public void setHourlyRate(Double hourlyRate) {
+        this.hourlyRate = hourlyRate;
+    }
 }
--- src/main/java/com/mvm/kitchenmanager/model/Role.java
+++ src/main/java/com/mvm/kitchenmanager/model/Role.java
@@ -1,4 +1,13 @@
 package com.mvm.kitchenmanager.model;
 
 public enum Role {
+    OWNER,
+    CHEF,
+    KITCHEN_MANAGER,
+    COOK,
+    WAITER,
+    HOST;
+    public void addRole(String role) {
+
+    }
 }
